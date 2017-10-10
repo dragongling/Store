ad49b56a84d76d4a523fc1f4368b0ce05ed6a1d1
Alexander Slesarenko <dragongling@gmail.com>
2017-10-10 16:15:48 +0500

    Games list functionality added

--- WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web.Mvc;
 using Moq;
 using Ninject;
+using Domain.Abstract;
+using Domain.Entities;
 
 namespace WebUI.Infrastructure
 {
@@ -29,7 +31,14 @@ namespace WebUI.Infrastructure
 
         private void AddBindings()
         {
-
+            Mock<IGameRepository> mock = new Mock<IGameRepository>();
+            mock.Setup(m => m.Games).Returns(new List<Game>
+            {
+                new Game { Name = "SimCity", Price = 1499 },
+                new Game { Name = "TITANFALL", Price = 2299 },
+                new Game { Name = "Battlefield 4", Price = 899.4M }
+            });
+            kernel.Bind<IGameRepository>().ToConstant(mock.Object);
         }
     }
 }
