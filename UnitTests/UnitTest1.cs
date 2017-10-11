4fcccacb9252d2be1e361b53ccc4c50b814c4ae0
SamMetalWorker <dragongling@gmail.com>
2017-10-11 11:56:21 +0500

    Bootstrap theme & CSS added

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -1,5 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Domain.Abstract;
+using Domain.Entities;
+using WebUI.Controllers;
+using WebUI.Models;
+using WebUI.HtmlHelpers;
 
 namespace UnitTests
 {
@@ -7,8 +16,74 @@ namespace UnitTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Can_Paginate()
         {
+            // Организация (arrange)
+            Mock<IGameRepository> mock = new Mock<IGameRepository>();
+            mock.Setup(m => m.Games).Returns(new List<Game>
+            {
+                new Game { GameID = 1, Name = "Игра1"},
+                new Game { GameID = 2, Name = "Игра2"},
+                new Game { GameID = 3, Name = "Игра3"},
+                new Game { GameID = 4, Name = "Игра4"},
+                new Game { GameID = 5, Name = "Игра5"}
+            });
+            GameController controller = new GameController(mock.Object);
+            controller.pageSize = 3;
+
+            // Действие (act)
+            GamesListViewModel result = (GamesListViewModel)controller.List(2).Model;
+
+            // Утверждение (assert)
+            List<Game> games = result.Games.ToList();
+            Assert.IsTrue(games.Count == 2);
+            Assert.AreEqual(games[0].Name, "Игра4");
+            Assert.AreEqual(games[1].Name, "Игра5");
+        }
+
+        [TestMethod]
+        public void Can_Generate_Page_Links()
+        {
+            HtmlHelper myHelper = null;
+            PagingInfo pagingInfo = new PagingInfo
+            {
+                CurrentPage = 2,
+                TotalItems = 28,
+                ItemsPerPage = 10
+            };
+
+            Func<int, string> pageUrlDelegate = i => "Page" + i;
+
+            MvcHtmlString result = myHelper.PageLinks(pagingInfo, pageUrlDelegate);
+
+            Assert.AreEqual(@"<a class=""btn btn-default"" href=""Page1"">1</a>"
+                + @"<a class=""btn btn-default btn-primary selected"" href=""Page2"">2</a>"
+                + @"<a class=""btn btn-default"" href=""Page3"">3</a>",
+                result.ToString());
+        }
+
+        [TestMethod]
+        public void Can_Send_Pagination_View_Model() {
+            Mock<IGameRepository> mock = new Mock<IGameRepository>();
+
+            mock.Setup(m => m.Games).Returns(new List<Game>
+            {
+                new Game{GameID = 1, Name = "Игра1"},
+                new Game{GameID = 2, Name = "Игра2"},
+                new Game{GameID = 3, Name = "Игра3"},
+                new Game{GameID = 4, Name = "Игра4"},
+                new Game{GameID = 5, Name = "Игра5"}
+            });
+            GameController controller = new GameController(mock.Object);
+            controller.pageSize = 3;
+
+            GamesListViewModel result = (GamesListViewModel)controller.List(2).Model;
+
+            PagingInfo pageInfo = result.PagingInfo;
+            Assert.AreEqual(pageInfo.CurrentPage, 2);
+            Assert.AreEqual(pageInfo.ItemsPerPage, 3);
+            Assert.AreEqual(pageInfo.TotalItems, 5);
+            Assert.AreEqual(pageInfo.TotalPages, 2);
         }
     }
 }
