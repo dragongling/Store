e71d092c35d6cd365b9a0096e5a4b51005aac289
SamMetalWorker <dragongling@gmail.com>
2017-10-11 08:50:46 +0500

    MS SQL Server connection added

---
+++ Domain/Concrete/EFGameRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Abstract;
+using Domain.Entities;
+
+namespace Domain.Concrete
+{
+    public class EFGameRepository : IGameRepository
+    {
+        EFDbContext context = new EFDbContext();
+
+        public IEnumerable<Game> Games {
+            get { return context.Games; }
+        }
+    }
+}
--- WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -5,7 +5,7 @@ using System.Web.Mvc;
 using Moq;
 using Ninject;
 using Domain.Abstract;
-using Domain.Entities;
+using Domain.Concrete;
 
 namespace WebUI.Infrastructure
 {
@@ -31,14 +31,7 @@ namespace WebUI.Infrastructure
 
         private void AddBindings()
         {
-            Mock<IGameRepository> mock = new Mock<IGameRepository>();
-            mock.Setup(m => m.Games).Returns(new List<Game>
-            {
-                new Game { Name = "SimCity", Price = 1499 },
-                new Game { Name = "TITANFALL", Price = 2299 },
-                new Game { Name = "Battlefield 4", Price = 899.4M }
-            });
-            kernel.Bind<IGameRepository>().ToConstant(mock.Object);
+            kernel.Bind<IGameRepository>().To<EFGameRepository>();
         }
     }
 }
